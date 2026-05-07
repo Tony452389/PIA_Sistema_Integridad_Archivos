34541be8440581fcc63f55ff444d263979dce27f
MoadSick <cibermarco8@gmail.com>
2026-05-07 09:25:22 -0600

    optimizar generación de hash usando flujos para mejorar eficiencia

--- src/hash_engine.cpp
+++ src/hash_engine.cpp
@@ -21,12 +21,17 @@ Abril 2026
 */
 
 #include "hash_engine.h"
+#include <fstream>
 
-unsigned long generateHash(const std::string& content){
+unsigned long generateHash(const std::string& filePath) {
+    std::ifstream file(filePath, std::ios::binary);
     unsigned long hash = 0;
-    
-    for(char c : content){
-        hash = hash *31 + c;
+    char c;
+
+    // Procesamos el archivo carácter por carácter (o por bloques) 
+    // sin cargarlo todo en un string gigante
+    while (file.get(c)) {
+        hash = hash * 31 + static_cast<unsigned char>(c); [cite: 11]
     }
 
     return hash;
